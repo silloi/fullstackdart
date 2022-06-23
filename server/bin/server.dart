e5c4dff5e4c6f3b7ee3ca5b6f1406cffcb683703
silloi <silloi1993@gmail.com>
2022-06-24 08:33:42 +0900

    adding websockets to the server

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -3,11 +3,14 @@ import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 // Configure routes.
 final _router = Router()
   ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  ..get('/echo/<message>', _echoHandler)
+  ..get('/ws', webSocketHandler(_handler));
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, Boring Show!\n');
@@ -18,12 +21,18 @@ Response _echoHandler(Request request) {
   return Response.ok('$message\n');
 }
 
+void _handler(WebSocketChannel webSocket) {
+  webSocket.stream.listen((message) {
+    webSocket.sink.add("echo $message");
+  });
+}
+
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final handler = Pipeline() // 
+  final handler = Pipeline() //
       .addMiddleware(logRequests())
       .addHandler(_router);
 
